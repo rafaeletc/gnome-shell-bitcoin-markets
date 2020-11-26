e3183d6336259f9e7e1b96a98dd53949eb0c341d
Rafael etc <rafaeletc@users.noreply.github.com>
2020-11-25 21:42:32 -0300

    Create ProviderNovadax.ts
    Adding NovaDAX exchange

---
+++ src/providers/ProviderNovadax.ts
@@ -0,0 +1,17 @@
+import * as BaseProvider from '../BaseProvider';
+
+export class Api extends BaseProvider.Api {
+  apiName = 'Binance';
+
+  apiDocs = [['API Docs', 'https://doc.novadax.com/en-US/#get-latest-ticker-for-specific-pair']];
+
+  interval = 15;
+
+  getUrl({ base, quote }) {
+    return `https://api.novadax.com/v1/market/ticker/?symbol=${base}_${quote}`;
+  }
+
+  getLast({ price }) {
+    return price;
+  }
+}
